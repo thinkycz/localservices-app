dddc31731ac32bb849000dd40e96857e908eeb7f
Leo Do <leo@pixel-mate.com>
2026-02-24 22:50:38 +0100

    Add UserSeeder with test admin user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'testovaci@admin.cz'],
+            [
+                'name' => 'Test Admin',
+                'password' => 'password',
+            ]
+        );
+    }
+}
